7af5e6847a5d12971475f4ace775ef48377b6101
fusion809 <brentonhorne77@gmail.com>
2016-03-19 03:40:36 +1000

    Initial commit

---
+++ archlive/airootfs/root/Shell/cd.sh
@@ -0,0 +1,37 @@
+function cda {
+	cd ~/archlive
+}
+
+function cdsh {
+	cd ~/Shell
+}
+
+function cdpm {
+	cd /etc/lightdm
+}
+
+function cdap {
+	cd /usr/share/applications
+}
+
+function cdx {
+	cd /usr/share/xsessions
+}
+
+function cdm {
+	cd /etc/xdg/menus
+}
+
+alias cdmen=cdm
+
+function cdd {
+	cd /usr/share/desktop-directories
+}
+
+function cdi {
+	cd /usr/share/icons
+}
+
+function cdp {
+	cd /etc/pacman.d/
+}
