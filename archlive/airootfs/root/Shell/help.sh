fab9be587976a416037b64a357bac59da27b9794
fusion809 <brentonhorne77@gmail.com>
2016-03-22 18:40:40 +1000

    Updating

---
+++ archlive/airootfs/root/Shell/help.sh
@@ -0,0 +1,9 @@
+function install-help {
+  echo 'The functions you need to install Arch Linux are:\n
+  * mountup
+  * pacsup
+  * genf
+  * hostn
+  * langn
+  * achroot'
+}
--- archlive/airootfs/root/Shell/pacstrap.sh
+++ archlive/airootfs/root/Shell/pacstrap.sh
@@ -15,3 +15,30 @@ function pacsup {
     echo "Please select one of the aforementioned options, either 1 or 2! Run `pacsup` again!"
   fi
 }
+
+function genf {
+  genfstab -p /mnt >> /mnt/etc/fstab
+}
+
+function hostn {
+  echo "What do you wish to call your PC?"
+  read NAME
+  echo "$NAME" >> /mnt/etc/hostname
+}
+
+function langn {
+  echo "What language locale do you wish your PC to use? e.g., This ISO uses en_US.UTF-8."
+  read LOCALE
+  echo "LANG=$LOCALE" >> /mnt/etc/locale.conf
+}
+
+function achroot {
+  printf 'In this Arch Chroot of your new system you will need to set your timezone by running:\n
+  ln -s /usr/share/zoneinfo/zone/subzone /etc/localtime\nFor example, for me in Queensland, Australia I would run\n
+  ln -s /usr/share/zoneinfo/Australia/Brisbane /etc/localtime\n
+  Then uncomment any locales you want in /etc/locale.gen (by removing the hashtag # before them) and run `locale-gen`.\n
+  Optionally you can set your keymap or font preferences in /etc/vconsole.conf, edit your /etc/mkinitcpio.conf file (after which you should run\n
+  `mkinitcpio -p linux`), set the root password (by running `passwd`), set up your user accounts (by running `useradd -m -g $USERNAME`),\n
+  install a desktop environment (with `pacman -S $META` where `$META` is the name of the DEs metapackage), etc.'
+  arch-chroot /mnt /bin/bash
+}
